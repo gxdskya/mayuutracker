d9682abe301d1d1c7170d5eb06325e6c28a7f315
gxdskaya <phantom.2008@yahoo.com>
2021-10-06 11:54:46 +0800

    commit the flutter ia v3

--- lib/FoodSearch/foodSearch.dart
+++ lib/FoodSearch/foodSearch.dart
@@ -274,41 +274,11 @@ class _EntryBoxState extends State<EntryBox> {
           TextButton(
               onPressed: () async{
 
-                  if(errorMessage ==""){
-
-                    List<foodDetails.Flavonoid> actualNutrients = [];
-                    double scaleFactor = numberOfGrams/100;
-                    double calories = scaleFactor*widget.flavanoidList[0].amount;
-                    widget.flavanoidList.removeAt(0);
-
-                    List<Map> uploadDataNutrients = [];
-
-                    print("ok");
-                    for (foodDetails.Flavonoid flavanoid in widget.flavanoidList){
-                      flavanoid.amount = flavanoid.amount*scaleFactor;
-                      Map newMap = {flavanoid.amount : flavanoid.unit};
-                      Map newMap2 = {flavanoid.name : newMap};
-                      uploadDataNutrients.add(newMap2);
-
-                    }
-                    print("ok");
-                    DateTime timestamp = new DateTime.now();
-
-                    String collectionName = loggedInUser.email+'sEntries'+timestamp.toString();
-                    print(collectionName);
 
 
                     //code to add the entire array and display calories separately
-                    try{
-                      _firestore.collection(collectionName).add({'user': loggedInUser.email.toString(), 'name': widget.foodName.toString(), 'calories': calories.toString(), 'nutrientList': uploadDataNutrients, 'timestamp': timestamp.toString()});
-                      print('ok');//i don't know this looks right???
+                  },
 
-                    }catch(e){
-                      print('go to');
-                    }
-                  }
-
-                },
               child: Text('ADD')),
         ],
       ),
